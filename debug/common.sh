4bdb01c46eddf926698b14a4753fcbcd5c41bffa
Hu,Xiaoming <huxiaoming@live.com>
2018-03-13 14:33:44 +0800

    more scripts

--- debug/common.sh
+++ debug/common.sh
@@ -8,6 +8,8 @@ build_dir=${src_dir}/build
 kernel_image=${build_dir}/arch/x86_64/boot/bzImage
 initrd_image=./initrd.img-4.13.0-36-generic
 rootfs_image=../rootfs/busybox/rootfs.img
+hda_image=../rootfs/qemu/linux-0.2.img
+hdb_image=../rootfs/qemu/debian_wheezy_amd64_standard.qcow2
 
 gdb_param=
 dbg_serial=/tmp/dbgserial
--- debug/qemu_hda.sh
+++ debug/qemu_hda.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash 
 source ./common.sh
 
 #qemu-system-x86_64 -nographic -no-kvm -kernel ${kernel_image} -initrd ${initrd_image} -smp 2 -gdb tcp::1234  -S
@@ -6,4 +6,4 @@ source ./common.sh
 #qemu-system-x86_64 -no-kvm -kernel ${kernel_image} -initrd ${rootfs_image} -smp 1 -gdb tcp::1234  -S
 #qemu-system-x86_64 -no-kvm -kernel ${kernel_image} -initrd ${rootfs_image} -append "root=/dev/ram rdinit=/bin/sh" -smp 1
 #qemu-system-x86_64 -no-kvm -kernel ${kernel_image} -hda ${hda_image}  -initrd ${rootfs_image}  -append "root=/dev/ram init=/bin/sh console=ttyS0,38400 debug" -smp 1 -serial file:serial.out
-qemu-system-x86_64 -no-kvm -kernel ${kernel_image} -hda ${hda_image} -hdb ${hdb_image}  -initrd ${rootfs_image}  -append "root=/dev/ram rdinit=/etc/init.d/rcS debug " -smp 1 -serial file:serial.out -display sdl -vga std -gdb tcp::1234  -S
+qemu-system-x86_64 -no-kvm -kernel ${kernel_image} -boot c -hda ${hdb_image} -append "root=/dev/hda console=ttyS0,38400 debug" -smp 1 -serial file:serial.out
