2b918ef14147348285d40e3bd7ac6d5a763d0faa
Hu,Xiaoming <huxiaoming@live.com>
2018-03-14 10:12:49 +0800

    vscode debug is ready

--- debug/common.sh
+++ debug/common.sh
@@ -37,3 +37,26 @@ gdb_secondrun()
     -ex 'set arch i386:x86-64' \
     -ex 'target remote localhost:1234'
 }
+
+qemu_start()
+{
+  QEMU_START_COMMAND='qemu-system-x86_64 -no-kvm -kernel ${kernel_image} -hda ${hda_image} -hdb ${hdb_image}  -initrd ${rootfs_image}  -append "root=/dev/ram rdinit=/etc/init.d/rcS debug " -smp 1 -serial file:serial.out -display sdl -vga std -gdb tcp::1234'
+
+  if [ $# -ne 1 ]
+  then
+    echo "run qemu in the normal mode: start qemu with -S and wait for the debugger"
+    echo "start qemu using command: ${QEMU_START_COMMAND} -S"
+    eval ${QEMU_START_COMMAND}' -S &'
+  else
+    if [ $1 == '1' ]; then 
+      echo "start qemu, then break at start_kernel"
+      eval ${QEMU_START_COMMAND}' -S &'
+      sleep 2s
+      gdb_firstrun
+    else 
+      echo "start qemu and boot"
+      eval ${QEMU_START_COMMAND}' &'
+    fi
+  
+  fi
+}
