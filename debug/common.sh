007f90b3f20dfbab279bb9a4054dd5ea9b955e02
Hu,Xiaoming <huxiaoming@live.com>
2018-04-19 14:18:08 +0800

    disable dirver cert virification when build linux

--- debug/common.sh
+++ debug/common.sh
@@ -3,7 +3,8 @@ ABSOLUTE_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)/$(basename "${BASH_
 dir_name=$(dirname $ABSOLUTE_PATH)
 echo "The scripth installer running at $dir_name"
 
-src_dir=../../../linux/src/linux-4.15.7
+src_dir=../../../linux/src/linux
+#src_dir=../../../linux/src/linux-2.6.39
 build_dir=${src_dir}/build
 kernel_image=${build_dir}/arch/x86_64/boot/bzImage
 initrd_image=./initrd.img-4.13.0-36-generic
